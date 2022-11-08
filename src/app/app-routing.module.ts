b71a691d7aae76c77a49dedafaf491446ab1305d
Meshraj Khatiwada <meshrajk@hotmail.com>
2022-11-07 19:46:26 -0500

    added auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GameComponent } from './components/game/game.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'dashboard',
